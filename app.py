9d786cdedc87b3bfe35ab120caccd7bb157740fd
SteveRodkiss <steve.rodkiss@gmail.com>
2023-08-13 14:42:16 +1200

    tested database

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello, World")
+import sqlite3
+
+db = sqlite3.connect("fighters.db")
+cursor = db.cursor()
+sql = "SELECT * from fighter;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
